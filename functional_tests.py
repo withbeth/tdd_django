f33b0422d03ee3d439c1243acdae3bf83f45e975
JongEun,Jeong | Brian <with.beth@gmail.com>
2018-09-17 22:28:26 +0900

    Update user story

--- functional_tests.py
+++ functional_tests.py
@@ -5,9 +5,26 @@ from selenium import webdriver
 # - How to confirm : Spin up Django's dev server(local) and actually see it serving up a web page by using selenium.
 
 browser = webdriver.Firefox()
+
+# Action : Check out its homepage.
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# Should browser title is To-Do.
+assert 'To-Do' in browser.title
+
+# Should be invited to enter a new to-do item.
+
+# Action : When user types and enter, Should page be updated and to-do list also be updated.
+
+# Should be invited to enter a new to-do item.
+
+# Should remember the user's to-do list (e.g, unique url for each users)
+
+# Action : User re-visit that url
+
+# Should the to-do list is still there.
+
+browser.quit()
 
 
 
